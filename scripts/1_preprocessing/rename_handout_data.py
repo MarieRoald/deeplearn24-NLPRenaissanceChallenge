e94fe09b5ae3808adba620813ebe25c60d1461ca
tita <tita.enstad@nb.no>
2024-07-22 12:32:27 +0200

    Update handout rename script (move both test and train)

--- scripts/1_preprocessing/rename_handout_data.py
+++ scripts/1_preprocessing/rename_handout_data.py
@@ -12,13 +12,21 @@ logger.setLevel(logging.DEBUG)
 
 def rename_handout_data():
     logger.info("Copying data from data/0_input/handout-created to data/0_input/handout-modified/")
-    modified_p = Path("data/0_input/handout-modified/words")
-    modified_p.mkdir(parents=True, exist_ok=True)
 
-    df = pd.read_csv("data/0_input/handout-created/outputs.csv")
-    df["from_path"] = df.FILENAME.apply(lambda x: f"data/0_input/handout-created/output/{x}")
-    df["image_path"] = [f"data/0_input/handout-modified/words/{i}.png" for i in df.index]
-    df["transcription"] = df.IDENTITY
+    df = pd.read_csv("data/0_input/handout-created/outputsTest.csv")
+    df["is_test"] = df.IDENTITY.apply(str.isnumeric)
+    df["from_path"] = df.FILENAME.apply(lambda x: f"data/0_input/handout-created/outputTest/{x}")
+    df["image_path"] = [
+        f"data/0_input/handout-modified/{'test' if e.is_test else 'train'}/{e.Index}.png"
+        for e in df.itertuples()
+    ]
+    df["transcription"] = ["" if e.is_test else e.IDENTITY for e in df.itertuples()]
+
+    modified_p = Path("data/0_input/handout-modified/")
+    test_p = modified_p / "test"
+    train_p = modified_p / "train"
+    test_p.mkdir(parents=True, exist_ok=True)
+    train_p.mkdir(parents=True, exist_ok=True)
 
     for e in tqdm(df.itertuples()):
         img = Path(e.from_path)
@@ -26,15 +34,20 @@ def rename_handout_data():
             logger.error(f"{img} does not exist")
             exit()
         copy(src=img, dst=e.image_path)
+        if not e.is_test:
+            transcription = e.transcription
 
-        transcription = e.transcription
+            txt_file = train_p / f"{Path(e.image_path).stem}.txt"
+            with txt_file.open("w+") as f:
+                f.write(transcription)
 
-        txt_file = modified_p / f"{Path(e.image_path).stem}.gt.txt"
-        with txt_file.open("w+") as f:
-            f.write(transcription)
-
-    df = df[["image_path", "transcription"]]
-    df.to_csv("data/0_input/handout-modified/handout.csv", index=False)
+    df[["image_path", "transcription"]].to_csv(
+        "data/0_input/handout-modified/handout.csv", index=False
+    )
+    df[~df.is_test][["image_path", "transcription"]].to_csv(
+        "data/0_input/handout-modified/train.csv", index=False
+    )
+    df[df.is_test][["image_path"]].to_csv("data/0_input/handout-modified/test.csv", index=False)
 
 
 if __name__ == "__main__":
