585d23a754a74c140efb6b861d8780600fb4a400
tita <tita.enstad@nb.no>
2024-07-23 11:16:41 +0200

    Write val and train splits from handout data

--- scripts/1_preprocessing/rename_handout_data.py
+++ scripts/1_preprocessing/rename_handout_data.py
@@ -14,19 +14,33 @@ def rename_handout_data():
     logger.info("Copying data from data/0_input/handout-created to data/0_input/handout-modified/")
 
     df = pd.read_csv("data/0_input/handout-created/outputsTest.csv")
-    df["is_test"] = df.IDENTITY.apply(str.isnumeric)
+
+    df_test = df[df.IDENTITY.apply(str.isnumeric)].copy()
+    df_test["split"] = ["test"] * len(df_test)
+
+    df_train = pd.read_csv("data/0_input/handout-created/train.csv")
+    df_train["split"] = ["train"] * len(df_train)
+
+    df_val = pd.read_csv("data/0_input/handout-created/valid.csv")
+    df_val["split"] = ["val"] * len(df_val)
+
+    df = pd.concat([df_test, df_train, df_val])
     df["from_path"] = df.FILENAME.apply(lambda x: f"data/0_input/handout-created/outputTest/{x}")
-    df["image_path"] = [
-        f"data/0_input/handout-modified/{'test' if e.is_test else 'train'}/{e.Index}.png"
-        for e in df.itertuples()
-    ]
-    df["transcription"] = ["" if e.is_test else e.IDENTITY for e in df.itertuples()]
+
+    df["image_path"] = df.apply(
+        lambda row: f"data/0_input/handout-modified/{row.split}/{row.name}.png", axis=1
+    )
+
+    df["transcription"] = [e.IDENTITY if e.split != "test" else "" for e in df.itertuples()]
 
     modified_p = Path("data/0_input/handout-modified/")
     test_p = modified_p / "test"
     train_p = modified_p / "train"
+    val_p = modified_p / "val"
+
     test_p.mkdir(parents=True, exist_ok=True)
     train_p.mkdir(parents=True, exist_ok=True)
+    val_p.mkdir(parents=True, exist_ok=True)
 
     for e in tqdm(df.itertuples()):
         img = Path(e.from_path)
@@ -34,7 +48,7 @@ def rename_handout_data():
             logger.error(f"{img} does not exist")
             exit()
         copy(src=img, dst=e.image_path)
-        if not e.is_test:
+        if not e.split == "test":
             transcription = e.transcription
 
             txt_file = train_p / f"{Path(e.image_path).stem}.txt"
@@ -44,10 +58,15 @@ def rename_handout_data():
     df[["image_path", "transcription"]].to_csv(
         "data/0_input/handout-modified/handout.csv", index=False
     )
-    df[~df.is_test][["image_path", "transcription"]].to_csv(
+    df[df.split == "train"][["image_path", "transcription"]].to_csv(
         "data/0_input/handout-modified/train.csv", index=False
     )
-    df[df.is_test][["image_path"]].to_csv("data/0_input/handout-modified/test.csv", index=False)
+    df[df.split == "val"][["image_path", "transcription"]].to_csv(
+        "data/0_input/handout-modified/val.csv", index=False
+    )
+    df[df.split == "test"][["image_path"]].to_csv(
+        "data/0_input/handout-modified/test.csv", index=False
+    )
 
 
 if __name__ == "__main__":
@@ -55,4 +74,4 @@ if __name__ == "__main__":
 
     rename_handout_data()
 
-    logger.info("Done")
+    logger.info("Done. See results in data/0_input/handout-modified")
