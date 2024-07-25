ea27cac90d44ed333e4fccc796b7aaef1504f724
tita <tita.enstad@nb.no>
2024-07-26 01:06:49 +0200

    Train tesseract models on line level data

--- scripts/4_inference/tesseract/inference.py
+++ scripts/4_inference/tesseract/inference.py
@@ -16,8 +16,7 @@ pil_logger = logging.getLogger("PIL")
 pil_logger.setLevel(logging.INFO)
 
 
-def tesseract_transcribe(model_name: str, input_csv: Path, output_csv: Path):
-    df = pd.read_csv(input_csv)
+def tesseract_transcribe(model_name: str, df: pd.DataFrame, output_csv: Path):
     predictions = [
         pytesseract.image_to_string(Image.open(e.file_name), lang=model_name)
         for e in tqdm(df.itertuples(), total=len(df))
@@ -25,6 +24,19 @@ def tesseract_transcribe(model_name: str, input_csv: Path, output_csv: Path):
     df["prediction"] = predictions
     output_csv.parent.mkdir(exist_ok=True, parents=True)
     df.to_csv(output_csv, index=False)
+
+
+def get_df(input_csv: Path, split: str = None) -> pd.DataFrame:
+    df = pd.read_csv(input_csv)
+    if split:
+        df = df[df.split == split]
+
+    if not all(df.file_name.apply(Path).apply(lambda x: x.exists())):
+        df["file_name"] = df.file_name.apply(lambda x: input_csv.parent / x)
+        if not all(df.file_name.apply(lambda x: x.exists())):
+            logger.error(f"Some source images do not exist")
+            exit()
+    return df
 
 
 if __name__ == "__main__":
@@ -38,15 +50,15 @@ if __name__ == "__main__":
         "--input_csv", type=Path, help=".csv-file containing image info ", required=True
     )
     parser.add_argument("--output_csv", type=Path, help="Output csv filename", required=True)
+    parser.add_argument(
+        "--split",
+        help="Data split to use (will filter out rows where column 'split' is not <split>)",
+    )
     args = parser.parse_args()
 
     setup_logging()
 
-    if not args.input_csv.exists():
-        logger.error(f"{args.input_csv} does not exist")
-        exit()
+    df = get_df(args.input_csv)
 
-    tesseract_transcribe(
-        model_name=args.model_name, input_csv=args.input_csv, output_csv=args.output_csv
-    )
+    tesseract_transcribe(model_name=args.model_name, df=df, output_csv=args.output_csv)
     logger.info(f"Done. See predictions at {args.output_csv}")
